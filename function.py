16e2cdd8fc3e17669788210421a591e995b154c8
lenapyagay <puageyhelen@gmail.com>
2024-11-27 13:37:27 +0300

    сделала ф-цию для извлечения названий товаров из документов

--- function.py
+++ function.py
@@ -2,11 +2,22 @@
 
 
 # TODO - import 
+from docx import Document
 
-def func1(n):
+def retrieves_goods(file_path): # функция для извлечения названий товаров из документа
     """" Функция 1 """
-    pass
+    document = Document(file_path)
+    product_names = []
+    for paragraph in document.paragraphs:
+        if paragraph.text.strip(): # проверка на наличие текста
+            if len(paragraph.text.split()) < 5: #название короче 5 слов
+                product_names.append(paragraph.text.strip())
+    return product_names
 
-def func2(n):
-    """" Функция 2 """
+
+
+def displays_names_in_a_table_format(product_names):
+    """"Выводит названия товаров в табличном виде"""
+    for i, name in enumerate(product_names, start=1):
+        print(f'{i}. {name}')
     pass
